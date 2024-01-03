0a23bd713ea370cc923438fe169c2a33ccaac6da
josephleejj <josephleejj@hotmail.sg>
2024-01-03 18:07:09 +0800

    add navbar and register routes for login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Navbar } from './components/NavBar';
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
-  
+
+  const {user} = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
         <div className='pages'>
           <Routes>
-            <Route
-              path="/"
-              element={<Home/>}>
-            </Route>
-            <Route path="/signup"
-              element={<Signup/>}
-            />
-            <Route path="/login"
-              element={<Login/>}
-            />
+            <Route 
+                path="/" 
+                element={user ? <Home /> : <Navigate to="/login" />} 
+              />
+              <Route 
+                path="/login" 
+                element={!user ? <Login /> : <Navigate to="/" />} 
+              />
+              <Route 
+                path="/signup" 
+                element={!user ? <Signup /> : <Navigate to="/" />} 
+              />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export const Navbar = () => {
+
+    const {logout} = useLogout()
+    const {user} = useAuthContext()
+
+
     return (
         <header>
             <div className="container">
@@ -8,10 +15,18 @@ export const Navbar = () => {
                     <h1>Home</h1>
                 </Link>
                 <nav>
-                    <div>
+                    {user && (
+                        <div>
+                            <span>{user.email}</span>
+                            <button onClick={logout}>Log Out</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
                         <Link to="/login">Login</Link>
                         <Link to="/signup">Signup</Link>
-                    </div>
+                        </div>
+                    )}
                 </nav>
                 
             </div>
